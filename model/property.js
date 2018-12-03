d5c677b36a010a66b8f7629d469627c5f556d8ce
topickering <topickering@gmail.com>
2018-12-03 14:33:36 +0000

    Property branch: basic property functions tested and written

--- model/property.js
+++ model/property.js
@@ -3,15 +3,27 @@
 function Property(owner) {
   this._owner = owner;
   this._name = '';
-  this._size = '';
+  this._bedrooms = '';
   this._price = '';
   this._photo = '';
 };
 
-Property.prototype.isAvailable = function(date) {
-  // check if this property is in the joiner table on that date
+// Property.prototype.isAvailable = function(date) {
+//   check if this property is in the joiner table on that date
+// };
+
+Property.prototype.updateName = function(name) {
+  this._name = name;
 };
 
 Property.prototype.updatePrice = function(price) {
   this._price = price;
+};
+
+Property.prototype.updateBedrooms = function(size) {
+  this._bedrooms = size;
+};
+
+Property.prototype.updatePhoto = function(photo) {
+  this._photo = photo;
 };
--- spec/property_spec.js
+++ spec/property_spec.js
@@ -1,7 +1,45 @@
 'use strict'
 
-describe ('Property', function() {
+describe('Property', function() {
 
+var property;
 
+  beforeEach(function() {
+    property = new Property('testOwner');
+  });
+
+  describe('can be created', function() {
+    it('with an owner property being stored', function() {
+      expect(property._owner).toEqual('testOwner')
+    });
+  });
+
+  describe('has a name property', function() {
+    it('which can be set with updateName', function() {
+      property.updateName('Cris');
+      expect(property._name).toEqual('Cris');
+    });
+  });
+
+  describe('has a price property', function() {
+    it('which can be set with updatePrice', function() {
+      property.updatePrice(100);
+      expect(property._price).toEqual(100);
+    });
+  });
+
+  describe('has a bedrooms property', function() {
+    it('which can be set with updateBedrooms', function() {
+      property.updateBedrooms(2);
+      expect(property._bedrooms).toEqual(2);
+    });
+  });
+
+  describe('has a Photo property', function() {
+    it('which can be set with updatePhoto', function() {
+      property.updatePhoto('testURL');
+      expect(property._photo).toEqual('testURL');
+    });
+  });
 
 });
