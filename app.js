a685d22b5e8a5251094a91c01a22b66a3d8f5c4a
kiedunne <kie.dunne@gmail.com>
2018-12-04 15:07:48 +0000

    Fixed app.js routing problem

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var router = express.Router();
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -21,9 +22,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+  /* GET home page. */
+  app.get('/', function(req, res) {
+    res.render('index', { title: 'APP TEST' });
+  });
+
+  /* GET signup page. */
+  app.get('/signup', function(req, res, next) {
+    res.render('signup', { title: 'APP TEST' });
+  });
+
+  /* POST signup page. */
+  app.post('/signup', function(req, res) {
+    User.signup({
+      email: req.body.email,
+      username: req.body.name,
+      password: req.body.password
+      }).then( function (result) {
+      res.redirect('/');
+      });
+    });
+
+  /* GET login page. */
+  app.get('/login', function(req, res, next) {
+    res.render('login', { title: 'APP TEST' });
+  });
+
+  app.listen(3000, function() {
+    console.log('Server started on port 3000');
+  })
 
 module.exports = app;
