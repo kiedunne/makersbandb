c40549450c54186b9910c77b107f947410c1898c
Cristopher Palacios <criszelaya24@hotmail.com>
2018-12-05 14:36:44 +0000

    Solving bux in app.js

--- app.js
+++ app.js
@@ -1,118 +1,34 @@
-var createError = require('http-errors');
-var express = require('express');
-var router = express.Router();
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var cookieSession = require("cookie-session");
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var database = require('database');
-var app = express();
+const express      = require('express');
+const bodyParser   = require('body-parser');
+const user         = require('./routes/user.route');
+const index        = require('./routes/index.route');
+const property = require('./routes/properties.route');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const path = require('path');
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+const mongoose  = require('mongoose');
 
-// Mongo Setup
-const mongoose = require('mongoose');
-
-const mongoDB = process.env.MONGODB_URI || database.url;
+let db_url = "mongodb://cesarecamurani:valhalla999@ds119164.mlab.com:19164/cesaredb";
+const mongoDB   = process.env.MONGODB_URI || db_url;
 mongoose.connect(mongoDB);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-
-// For validations
-var expressValidator = require('express-validator');
-const { check, validationResult } = require('express-validator/check');
-
-// Global Variable
-app.use(function(req, resp, next){
-
-  resp.locals.errors = null;
-  resp.locals.title = null;
-
-  next();
-});
-
-// View engine setup
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(__dirname + '/public'));
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieSession({
+  secret: 'work hard',
+}));
+app.use('/', index);
+app.use('/users', user);
+app.use('/properties', property);
 
-  /* GET home page. */
-  app.get('/', function(req, res) {
-    res.render('index', { title: 'APP TEST' });
-  });
-
-  /* GET signup page. */
-  app.get('/users', function(req, res, next) {
-    res.render('signup', { title: 'APP TEST', errors: '' });
-  });
-
-  /* POST signup page. */
-  app.post('/users/new', [check('name').not().isEmpty().withMessage('First Name is emty'),
-    check('surname').not().isEmpty().withMessage('Second Name is emty'),
-    check('username').not().isEmpty().withMessage('User Name is emty'),
-    check('email').isEmail().withMessage('Not an email'),
-    check('password').isLength({ min: 5 }).withMessage('Minimun length for password are 5 character')] , (req, resp) =>{
-
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-
-    resp.render('signup', {
-
-      title: 'Makers B&B',
-      errors: errors.array()
-    });
-  } else {
-  var user;
-  // Insertion in database
-  user = {
-    first_name: req.body.first_name,
-    second_name: req.body.second_name,
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  }
-  console.log(user);
-  resp.redirect('/');
-  }
+let port = 3000;
+app.listen(port, () => {
+    console.log('Server is up and running on port number ' + port);
 });
-
-  /* GET login page. */
-  app.get('/login', function(req, res, next) {
-    res.render('login', { title: 'APP TEST' });
-  });
-
-
-  //post login page
-
-  app.post('/login', [check('username').not().isEmpty().withMessage('User name empty'),
-    check('password').isLength({ min: 5 }).withMessage('Minimun length for password are 5 character')] , function(req, resp){
-    const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    resp.render('signup', {
-      title: 'Makers B&B',
-      errors: errors.array()
-    });
-  } else {
-  var user;
-  // INSERTION IN THE DATA BASE
-  user = {
-    username: req.body.username,
-    password: req.body.password
-  }
-  console.log(user);
-  resp.redirect('/');
-  }
-  });
-
-  app.listen(3000, function() {
-
-    console.log('Server started on port 3000');
-    })
-
-module.exports = app;
--- routes/index.route.js
+++ routes/index.route.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', { title: 'APP TEST' });
 });
 
 router.get('/users', function(req, res) {
-  res.render('signup');
+  res.render('signup', { title: 'APP TEST', errors: '' });
 });
 
 router.get('/login', function(req, res) {
